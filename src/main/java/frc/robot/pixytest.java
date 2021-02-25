e8ec9535521207ae0aef44e4e619563d286b6a35
Rohit Rajagopalan <rohitgan2004@gmail.com>
2021-02-25 10:38:47 -0500

    Pixy commit # 1

---
+++ src/main/java/frc/robot/pixytest.java
@@ -0,0 +1,14 @@
+/*package frc.robot;
+
+public class PixyCameraExample {
+
+	private static final Pixy2 pixy;
+
+	public static void initialize() {
+		pixy = Pixy2.createInstance(new SPILink()); // Creates a new Pixy2 camera using SPILink
+		pixy.init(); // Initializes the camera and prepares to send/receive data
+		pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
+		pixy.setLED(200, 30, 255); // Sets the RGB LED to purple
+	}
+}
+*/
